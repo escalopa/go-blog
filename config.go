666a8e5fe56a9d1b46bb9f01483ef2853c732730
escalopa <ahmad.helaly.dev@gmail.com>
2022-10-22 21:02:14 +0300

    add parse config func

---
+++ config.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/spf13/viper"
+	"log"
+)
+
+type Config struct {
+	Port             string `mapstructure:`
+	ConnectionString string ``
+}
+
+var AppConfig Config
+
+func LoadConfiguration() {
+	log.Println("Loading Configuration Data")
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yml")
+	viper.SetConfigName("config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = viper.Unmarshal(&AppConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
